a6a18b270a878ad9e8b11341238e6957a82e8226
o1egs <tankistonline21@gmail.com>
2024-05-14 13:43:03 +0400

    init

---
+++ src/main/java/ru/bebriki/sstusecurity/SstuSecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.bebriki.sstusecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstuSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstuSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bebriki/sstusecurity/SstuSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bebriki.sstusecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SstuSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
